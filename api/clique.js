25b6a751fa29ff9eed7ab28a128f4301e5cf0c99
Thadeu Cavalcanti <thadeu.ct@gmail.com>
2025-09-25 20:31:36 -0300

    Separação dos cabelhos

--- api/clique.js
+++ api/clique.js
@@ -1,14 +1,6 @@
 import { sql } from '@vercel/postgres';
 
 export default async function handler(request, response) {
-  // Adiciona os cabeçalhos CORS
-  response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (request.method === 'OPTIONS') {
-    return response.status(200).end();
-  }
 
   const { id } = request.query;
 
